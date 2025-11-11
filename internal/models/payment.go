29115a3600809791cab85b7719ed3891b8fb4726
ngxvu <xvung126@gmail.com>
2025-11-11 11:12:37 +0700

    Add payment service and repository implementation with CreatePayment functionality

--- internal/models/payment.go
+++ internal/models/payment.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"payment/pkg/http/utils"
+)
 
 type Payment struct {
 	BaseModel
@@ -8,9 +11,28 @@ type Payment struct {
 	CustomerID uuid.UUID `gorm:"type:uuid;index"`
 	Amount     float64   `gorm:"type:numeric(10,2)"`
 	Status     string    `gorm:"type:varchar(50);index"`
-	Currency   string    `gorm:"type:varchar(10)"`
 }
 
 func (Payment) TableName() string {
 	return "payments"
+}
+
+type CreatePaymentRequest struct {
+	OrderID    uuid.UUID `json:"order_id" binding:"required"`
+	CustomerID uuid.UUID `json:"customer_id" binding:"required"`
+	Amount     float64   `json:"amount" binding:"required"`
+	Status     string    `json:"status" binding:"required"`
+}
+
+type CreatePaymentResponseData struct {
+	PaymentID  uuid.UUID `json:"payment_id"`
+	OrderID    uuid.UUID `json:"order_id"`
+	CustomerID uuid.UUID `json:"customer_id"`
+	Amount     float64   `json:"amount"`
+	Status     string    `json:"status"`
+}
+
+type CreatePaymentResponse struct {
+	Meta *utils.MetaData           `json:"meta"`
+	Data CreatePaymentResponseData `json:"data"`
 }
--- internal/repositories/payment.go
+++ internal/repositories/payment.go
@@ -1,1 +1,54 @@
 package repositories
+
+import (
+	"context"
+	"gorm.io/gorm"
+	model "payment/internal/models"
+	pgGorm "payment/internal/repositories/pg-gorm"
+	"payment/pkg/http/utils"
+)
+
+type PaymentRepository struct {
+	db pgGorm.PGInterface
+}
+
+func NewPaymentRepository(newPgRepo pgGorm.PGInterface) *PaymentRepository {
+	return &PaymentRepository{db: newPgRepo}
+}
+
+type PaymentRepoInterface interface {
+	CreatePayment(ctx context.Context, tx *gorm.DB, PaymentRequest *model.CreatePaymentRequest) (*model.CreatePaymentResponse, error)
+}
+
+func (a *PaymentRepository) CreatePayment(ctx context.Context, tx *gorm.DB, paymentRequest *model.CreatePaymentRequest) (*model.CreatePaymentResponse, error) {
+
+	var cancel context.CancelFunc
+	if tx == nil {
+		tx, cancel = a.db.DBWithTimeout(ctx)
+		defer cancel()
+	}
+
+	paymentRecord := &model.Payment{
+		OrderID:    paymentRequest.OrderID,
+		CustomerID: paymentRequest.CustomerID,
+		Amount:     paymentRequest.Amount,
+		Status:     paymentRequest.Status,
+	}
+
+	if err := tx.Create(paymentRecord).Error; err != nil {
+		return nil, err
+	}
+
+	response := &model.CreatePaymentResponse{
+		Meta: utils.NewMetaData(ctx),
+		Data: model.CreatePaymentResponseData{
+			PaymentID:  paymentRecord.ID,
+			OrderID:    paymentRecord.OrderID,
+			CustomerID: paymentRecord.CustomerID,
+			Amount:     paymentRecord.Amount,
+			Status:     paymentRecord.Status,
+		},
+	}
+
+	return response, nil
+}
